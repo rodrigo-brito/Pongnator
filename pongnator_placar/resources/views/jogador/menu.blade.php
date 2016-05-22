de059655ceb8f0da09671fe4472c657cbb91b102
marco <marco.aureliosoares@hotmail.com>
2016-05-22 12:17:47 -0300

    teste

--- pongnator_placar/resources/views/jogador/menu.blade.php
+++
@@ -1,8 +0,0 @@
-
-<div class="row">
-    <div class="col-md-12">
-        <a href="{{ route("jogador.cadastrar") }}" class="btn btn-raised btn-primary">Novo</a>
-        <a href="{{ route("jogador") }}" class="btn btn-raised btn-primary">Listar</a>
-    </div>
-</div>
-
